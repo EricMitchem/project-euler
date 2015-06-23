5350f4daf64169e9ed05e81a5b501802e4adfe6e
Eric Mitchem <EricMitchem@gmail.com>
2015-06-23 14:54:56 -0500

    Problem 6

---
+++ problem6.fs
@@ -0,0 +1,9 @@
+﻿module EricMitchem.ProjectEuler.Problem6
+
+let problem() =
+    let rec impl acc sum sumSq =
+        match () with
+        | _ when acc > 100 -> (sum * sum) - sumSq
+        | _ -> impl (acc + 1) (sum + acc) (sumSq + acc * acc)
+
+    impl 1 0 0
