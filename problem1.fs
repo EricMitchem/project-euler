327d5908233325d0264fb9221de592378f4d961f
Eric Mitchem <EricMitchem@gmail.com>
2015-06-23 00:49:53 -0500

    Problem 1

---
+++ problem1.fs
@@ -0,0 +1,10 @@
+﻿module EricMitchem.ProjectEuler.Problem1
+
+let problem() =
+    let rec impl acc sum =
+        match acc with
+        | 1000 -> sum
+        | num when acc % 3 = 0 || acc % 5 = 0 -> impl (acc + 1) (sum + num)
+        | _ -> impl (acc + 1) sum
+
+    impl 3 0
