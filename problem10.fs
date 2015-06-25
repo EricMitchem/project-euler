486abde1c2c77acb7ab0eca8259ac0e2d0b83715
Eric Mitchem <EricMitchem@gmail.com>
2015-06-25 13:56:54 -0500

    Problem 10

---
+++ problem10.fs
@@ -0,0 +1,41 @@
+﻿module EricMitchem.ProjectEuler.Problem10
+
+open System
+
+// Returns a BitArray with all primes set.
+let sieveOfEratosthenes (n : int) : Collections.BitArray =
+    let arr = new Collections.BitArray(n + 1, true)
+    arr.Set(0, false)
+    arr.Set(1, false)
+
+    let succ p =
+        let rec impl acc =
+            match arr.Get(acc) with
+            | true -> acc
+            | _ -> impl (acc + 1)
+
+        impl (p + 1)
+
+    let rec mark p acc n =
+        match () with
+        | _ when p * p > n -> arr
+        | _ ->
+            match () with
+            | _ when acc > n -> let sp = succ p in mark sp (sp * sp) n
+            | _ -> arr.Set(acc, false)
+                   mark p (acc + p) n
+
+    mark 2 4 n
+
+let problem() =
+    let sieve = sieveOfEratosthenes 2000000
+
+    let rec impl idx sum =
+        match () with
+        | _ when idx > 2000000 -> sum
+        | _ ->
+            match sieve.Get(idx) with
+            | true -> impl (idx + 2) (sum + int64 idx)
+            | _ -> impl (idx + 2) sum
+
+    impl 3 2L
